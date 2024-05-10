63601b0bb57b691a7436ca0832a908ae4727dc0b
biraj <virajbogati11@gmail.com>
2024-05-10 17:48:56 +0545

    login screen

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -10,6 +10,8 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Text("Login"),
+    );
   }
 }
