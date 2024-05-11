ca1d236cd355d3f56448589a5f6175584ad14c79
biraj <virajbogati11@gmail.com>
2024-05-11 18:36:16 +0545

    splash screen

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -6,6 +6,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      body: Center(child: Text("ARTIFY"),),
       
     );
   }
