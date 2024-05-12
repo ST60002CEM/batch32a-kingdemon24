2cc8f95d10cc0a8086fcb76aa6847b49d3872c4f
biraj <virajbogati11@gmail.com>
2024-05-12 22:38:53 +0545

    loding in splash screen

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,13 +1,34 @@
+import 'package:final_assignment/screen/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("ARTIFY"),),
-      
+    // Adding a Future.delayed to simulate a loading process
+    Future.delayed(Duration(seconds: 2), () {
+      // Navigate to the next page after 2 seconds
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen()), // Replace NextPage() with your next page
+      );
+    });
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "ARTIFY",
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(), // Adding a CircularProgressIndicator
+          ],
+        ),
+      ),
     );
   }
 }
