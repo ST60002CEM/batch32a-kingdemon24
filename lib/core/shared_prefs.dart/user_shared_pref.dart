76791a2768f4e1babea7cfab980e1d49c52f296b
biraj <virajbogati11@gmail.com>
2024-06-18 10:27:13 +0545

    userprefrance

---
+++ lib/core/shared_prefs.dart/user_shared_pref.dart
@@ -0,0 +1,33 @@
+import 'package:dartz/dartz.dart';
+import 'package:final_assignment/core/failure/failure.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final userSharedPrefsProvider = Provider<UserSharedPrefs>((ref) {
+  return UserSharedPrefs();
+});
+
+class UserSharedPrefs {
+  late SharedPreferences _sharedPreferences;
+  // Set user token
+  Future<Either<Failure, bool>> setUserToken(String token) async {
+    try {
+      _sharedPreferences = await SharedPreferences.getInstance();
+      await _sharedPreferences.setString('token', token);
+      return right(true);
+    } catch (e) {
+      return left(Failure(error: e.toString()));
+    }
+  }
+
+  // Get user token
+  Future<Either<Failure, String?>> getUserToken() async {
+    try {
+      _sharedPreferences = await SharedPreferences.getInstance();
+      final token = _sharedPreferences.getString('token');
+      return right(token);
+    } catch (e) {
+      return left(Failure(error: e.toString()));
+    }
+  }
+}
