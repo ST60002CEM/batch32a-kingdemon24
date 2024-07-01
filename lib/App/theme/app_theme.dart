f10ec921b62dffbce51e5296c22041603858e54f
biraj <virajbogati11@gmail.com>
2024-07-01 15:04:44 +0545

    app theme

---
+++ lib/App/theme/app_theme.dart
@@ -0,0 +1,81 @@
+import 'package:final_assignment/app/constants/theme_constant.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static getApplicationTheme(bool isDark) {
+    return ThemeData(
+      // change the theme according to the user preference
+      colorScheme: isDark
+          ? const ColorScheme.dark(
+              primary: ThemeConstant.darkPrimaryColor,
+            )
+          : const ColorScheme.light(
+              primary: Color.fromARGB(255, 99, 16, 154),
+            ),
+      brightness: isDark ? Brightness.dark : Brightness.light,
+      fontFamily: 'Montserrat',
+      useMaterial3: true,
+
+      // Change app bar color
+      appBarTheme: const AppBarTheme(
+        elevation: 0,
+        backgroundColor: ThemeConstant.appBarColor,
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+        ),
+      ),
+
+      // Change elevated button theme
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          foregroundColor: Colors.white,
+          backgroundColor: ThemeConstant.primaryColor,
+          textStyle: const TextStyle(
+            fontSize: 20,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+
+      // Change text field theme
+      inputDecorationTheme: const InputDecorationTheme(
+        contentPadding: EdgeInsets.all(15),
+        border: OutlineInputBorder(),
+        labelStyle: TextStyle(
+          fontSize: 20,
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.red,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: ThemeConstant.primaryColor,
+          ),
+        ),
+      ),
+      // Circular progress bar theme
+      progressIndicatorTheme: const ProgressIndicatorThemeData(
+        color: ThemeConstant.primaryColor,
+      ),
+      //Bottom navigation bar theme
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: Color.fromARGB(255, 111, 3, 161),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        elevation: 0,
+      ),
+      drawerTheme: const DrawerThemeData(
+          backgroundColor: Color.fromARGB(255, 231, 194, 239)),
+    );
+  }
+}
