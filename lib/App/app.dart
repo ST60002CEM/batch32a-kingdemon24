2208710bc662daa4046d47454a871d4ec39c7ebe
biraj <virajbogati11@gmail.com>
2024-07-04 15:11:12 +0545

    common dialog box

--- lib/App/app.dart
+++ lib/App/app.dart
@@ -1,17 +1,20 @@
 import 'package:final_assignment/app/navigator_key/navigator_key.dart';
+import 'package:final_assignment/app/theme/app_theme.dart';
+import 'package:final_assignment/core/common/provider/theme_provider.dart';
 import 'package:final_assignment/features/splash/presentation/view/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'theme/theme_data.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,WidgetRef ref) {
+    final isDarkTheme = ref.watch(themeViewModelProvider);
     return MaterialApp(
       navigatorKey: AppNavigator.navigatorKey,
       debugShowCheckedModeBanner: false,
-      theme: getApplicationTheme(),
+      theme: AppTheme.getApplicationTheme(isDarkTheme),
       home: const SplashScreen(),
     );
   }
