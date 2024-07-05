ea42f6e9f1298e183d9a220db513d2f4caf46fd9
biraj <virajbogati11@gmail.com>
2024-07-05 15:14:45 +0545

    pagination

--- lib/App/constants/api_endpoint.dart
+++ lib/App/constants/api_endpoint.dart
@@ -15,12 +15,26 @@
 // }
 class ApiEndpoints {
   ApiEndpoints._();
-  static const Duration connectionTimeout = Duration(seconds: 5000);
+  static const Duration connectionTimeout = Duration(seconds: 50000);
   static const Duration receiveTimeout = Duration(seconds: 3000);
-  // static const String baseUrl = "http://192.168.1.74:5500/api/";
+  // static const String baseUrl = "http://192.168.137.1:5500/api/";
+  // static const String baseUrl = "http://10.0.2.2:5500/api/";
   static const String baseUrl = "http://192.168.1.74:5500/api/";
 
   //auth routes
   static const String login = "user/login";
   static const String register = "user/create";
+
+  static const String getMe = "user/getMe";
+  static const String verifyUser = "user/profile";
+  static const String getToken = "user/generate_token";
+  static const String paginatonProducts = "product/pagination";
+
+  // product routes
+  // static const String allproduct = "product/get_all_products";
+  static const String getAllProducts = 'product/get_all_products';
+  static const String updateProduct = 'product/update_product';
+  // static const String productImage = "http://10.0.2.2:5500/products/";
+  // static const String productImage = "http://192.168.137.1:5500/products/";
+  static const String productImage = "http://192.168.1.74:5500/products/";
 }
